0663ace3e6a817af3d4e855472f64920338530bb
ms <ms@mss-iMac.home>
2017-09-11 23:40:39 +0200

    untested but mostly reflects original code

--- R/jagst.R
+++ R/jagst.R
@@ -19,12 +19,19 @@ JAGSTself <- function(array,design,inds){
 	ob <- limma::lmFit(array,design=des)
 	obmod <- limma::eBayes(ob)
 	statsCor <- obmod$t[,2]
-	# unmoderated for self-contained test
+	# could use unmoderated for self-contained test
+	
+	cov_mat <- cor(t(array[inds,])) # QR instead
+
+	if(kappa(cov_mat)>1500) {
+		increa <- svd(cov_mat)$d[1]/1500 
+		cov_mat <- cov_mat + diag(increa,dim(cov_mat)[1])
+		}	
 
 	test_stat_cor <- t(statsCor[inds])%*%solve(cov_mat[inds,inds])%*%(statsCor[inds])
 	
 	p_self_cor <- pchisq(test_stat_cor[1],df=length(inds),lower.tail=F)
-	return(p_self_cor)
+	return(p_self_cor)	
 }
 
 
@@ -37,67 +44,48 @@ JAGSTself <- function(array,design,inds){
 #' @param array expression array
 #' design design matrix (intercept included by default)
 #' inds indices of transcripts to be tested
+#' its number of iterations for null generation.  This number times num_nest is the total number of samples for the null.
+#' num_nest number of samples within each null iteration.  Cheap computationally, but decreasing returns efficiency gain.
 #' @keywords 
 #' @export
 #' @examples
-#' JAGSTcomp(arr,des,ind)
+#' JAGSTcomp(arr,des,ind,its,num_nest)
  
-JAGSTcomp <- function(array,design,inds){
+JAGSTcomp <- function(array,design,inds,its=200,num_nest=2000){
     require(limma)
     require(selectiveInference)
-        
+
+    len_inds <- length(inds)
+	dim_x <- dim(array)[1]
+    
     des <- cbind(1,design)
 	ob <- limma::lmFit(array,design=des)
 	obmod <- limma::eBayes(ob)
 	statsCor <- obmod$t[,2]
-	# unmoderated for self-contained test
+
+	cov_mat <- cor(t(array[inds,])) # QR instead
+
+	if(kappa(cov_mat)>1500) {
+		increa <- svd(cov_mat)$d[1]/1500 
+		cov_mat <- cov_mat + diag(increa,dim(cov_mat)[1])
+		}	
 
 	test_stat_cor <- t(statsCor[inds])%*%solve(cov_mat[inds,inds])%*%(statsCor[inds])
-	te_null_cor <- JAGST::easier_null(len_inds,dim_x,its,num_nest,cancer_cor)
 
-	
-	
-	
-	
-	p_self_cor <- pchisq(test_stat_cor[1],df=length(inds),lower.tail=F)
-	return(p_self_cor)
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-#' Calculate null distribution for JAGST competitive test
-#'
-#' null distribution competitive test
-#' @param array expression array
-#' design design matrix (intercept included by default)
-#' inds indices of transcripts to be tested
-#' @keywords 
-easier_null <- function(len_inds,dim_x,its,num_nest,cancer_cor){
+	mat_trans <- t(array)
 	stat_nulls <- list()
 	for(ee in 1:its){
 	null_sam_inds <- sample(1:dim_x,len_inds,replace=F)
 	
-	larfit = lar(mat_trans[,null_sam_inds],cancer_cor) # is there a way to stop the algorithm early in general since aic will likely be achieved early in the calculation
+	larfit = lar(mat_trans[,null_sam_inds],design) # is there a way to stop the algorithm early in general since aic will likely be achieved early in the calculation
 	  out.aic <- larInf(larfit,type="aic")
 	  ncps_cor <- round(out.aic$sign * out.aic$vmat %*% out.aic$y/(out.aic$sigma * sqrt(rowSums(out.aic$vmat^2))),3) # test stats
 		
-	  stat_nulls[[ee]] <- rchisq(num_nest,df=length(inds1),ncp=sum(ncps_cor^2)) ## in these simulations would generally be assuming same size of correlated and uncorrelated test set so should not necessarily need to change df parameter but doing so here to prevent future issues
+	  stat_nulls[[ee]] <- rchisq(2000,df=length(inds),ncp=sum(ncps_cor^2)) ## in these simulations would generally be assuming same size of correlated and uncorrelated test set so should not necessarily need to change df parameter but doing so here to prevent future issues
 		}
-		return(unlist(stat_nulls))	
+	
+	comp_test_p <- calc_emp_p(unlist(stat_nulls,test_stat_cor[1])
+	return(comp_test_p)	
 	}
 
 
@@ -114,3 +102,7 @@ calc_emp_p <- function(dist,num){
 	pval <- 1-plac/(L+2)
 	return(pval)	
 	}
+	
+	
+	
+	
